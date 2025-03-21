1982ebccc5ccb35f96e3dffd9624318a5d9d1e26
soala144 <soala144@gmail.com>
2025-03-20 19:16:54 -0800

    Done with input section UI

--- src/Components/InputSection.jsx
+++ src/Components/InputSection.jsx
@@ -12,10 +12,10 @@ const InputSection = () => {
       <div className="w-3/4 p-4 flex justify-between items-center rounded-lg m-auto">
         <input
           type="text"
-          placeholder="place a URL"
-          className="bg-white w-3/4 h-10 rounded p-2"
+          placeholder="Shorten URL here ..."
+          className="bg-white w-[82%] h-10 rounded px-3"
         />
-        <Buttons borderRadius="4px" className="w-2/5">
+        <Buttons borderRadius="4px" className="w-[5%]">
           Shorten URL
         </Buttons>
       </div>
