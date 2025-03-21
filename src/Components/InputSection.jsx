6ed613a81a0cf79cbde515a584d3b22e09702030
soala144 <soala144@gmail.com>
2025-03-20 16:10:55 -0800

    h

--- src/Components/InputSection.jsx
+++ src/Components/InputSection.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import Buttons from "./Buttons";
 
 const InputSection = () => {
-  //   return (
-  //     // <div className="w-3/4 m-auto ">
-  //     //   <div className="w-full bg-white p-4 flex justify-between items-center rounded-lg m-auto">
-  //     //     <input type="text" placeholder="place a URL" />
-  //     //     <Buttons borderRadius="4px">Shorten URL</Buttons>
-  //     //   </div>
-  //     // </div>
-  //   );
+  return (
+    <div
+      className="w-3/4 m-auto "
+      style={{
+        backgroundImage: "url('src/assets/images/bg-shorten-desktop.svg')",
+      }}
+    >
+      <div className="w-3/4 p-4 flex justify-between items-center rounded-lg m-auto">
+        <input type="text" placeholder="place a URL" className="bg-gray" />
+        <Buttons borderRadius="4px">Shorten URL</Buttons>
+      </div>
+    </div>
+  );
 };
 
 export default InputSection;
