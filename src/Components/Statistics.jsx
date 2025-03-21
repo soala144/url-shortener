cb0a1bbca224ce087b758cbcbb78ba44ebc81737
soala144 <soala144@gmail.com>
2025-03-20 19:50:50 -0800

    Correction

--- src/Components/Statistics.jsx
+++ src/Components/Statistics.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Statistics = () => {
   return (
-    <div className="w-3/4 m-auto p-20">
+    <div className="w-3/4 m-auto pt-30 ">
       <div className="items-center flex flex-col">
         <h2 className="text-primary  font-black text-3xl mb-2">
           Advanced Statistics
         </h2>
         <p className="text-gray-300 text-center">
-          Track how your links are performing across the web <br /> with our
+          Track how your links are performing across the web with our <br />
           advanced statistics dashboard.
         </p>
       </div>
