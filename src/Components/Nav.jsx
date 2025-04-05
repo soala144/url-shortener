cc50718ac6ec4216e662ccf4aa8740741d6aa7cd
soala144 <soala144@gmail.com>
2025-04-05 08:00:26 -0800

    Responsiveness v1

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+import Buttons from "./Buttons";
 
-const Nav = () => {
+const Nav = ({ isOpen }) => {
   return (
-    <ul className="flex justify-between gap-4 text-gray-300 text-xs py-2">
-      <li>Features</li>
-      <li>Pricing</li>
-      <li>Resources</li>
-    </ul>
+    <>
+      <ul className="hidden md:flex justify-between gap-4 text-gray-300 text-xs py-2 ">
+        <li>Features</li>
+        <li>Pricing</li>
+        <li>Resources</li>
+      </ul>
+
+      {isOpen && (
+        <ul className="md:hidden flex justify-between gap-4 flex-col text-gray-300 text-xs py-2 ">
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Resources</li>
+          <Buttons bgColor="#fff" textColor=" #34313d">
+            Login
+          </Buttons>
+          <Buttons>Sign Up</Buttons>
+        </ul>
+      )}
+    </>
   );
 };
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./Logo";
 import Buttons from "./Buttons";
 import Nav from "./Nav";
-
+import { Menu, X } from "lucide-react";
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className=" w-3/4 flex justify-between items-center mt-6 bg-white m-auto">
       <div className="flex justify-between gap-4">
         <Logo />
-        <Nav />
+        <Nav isOpen={isOpen} />
       </div>
-      <div className="flex gap-2 py-2">
+      <button onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <X size={24} /> : <Menu size={24} />}
+      </button>
+      <div className="hidden md:flex gap-2 py-2">
         <Buttons bgColor="#fff" textColor=" #34313d">
           Login
         </Buttons>
