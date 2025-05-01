1321789bd7e06a7cc5d79a732274b243a05a1a58
soala144 <soala144@gmail.com>
2025-05-01 14:13:43 -0800

    updated

--- src/Components/Statistics.jsx
+++ src/Components/Statistics.jsx
@@ -25,7 +25,7 @@ const Statistics = () => {
   return (
     <div className="w-3/4 m-auto pt-30 ">
       <div className="items-center flex flex-col">
-        <h2 className="text-primary  font-black text-3xl mb-2">
+        <h2 className="text-primary text-center font-black text-2xl md:text-3xl mb-2">
           Advanced Statistics
         </h2>
         <p className="text-gray-300 text-center">
